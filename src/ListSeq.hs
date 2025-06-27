0c98203c68502fff95acec5852adb3fc965192a7
AngeloAlvarez18 <alvarezangelo92@gmail.com>
2025-06-27 16:06:23 -0300

    Scan y reduce de listas

--- src/ListSeq.hs
+++ src/ListSeq.hs
@@ -1,5 +1,6 @@
 module ListSeq where
 import Seq
+import Par
 
 instance Seq [] where
     emptyS :: [a]
@@ -57,14 +58,36 @@ instance Seq [] where
     joinS (x:xs) = x ++ joinS xs
 
     reduceS :: (a -> a -> a) -> a -> [a] -> a
-    reduceS _ b [] = b
-    reduceS f b (x:xs) = f x (reduceS f b xs)
+    reduceS f e [] = e
+    reduceS f e [x] = f e x
+    reduceS f e xs = let
+                      ct = contract f xs
+                    in reduceS f e ct
 
     scanS :: (a -> a -> a) -> a -> [a] -> ([a], a)
-    scanS _ b [] = ([], b)
-    scanS f b (x:xs) = let (ys, r) = scanS f (f b x) xs
-                       in (b : ys, r)
+    scanS f e []  = (emptyS, e)
+    scanS f e [x] = (singletonS e, f e x)
+    scanS f e xs  = let 
+                        (s',r) = scanS f e ct
+                        ct = contract f xs 
+                    in (expand f s' xs, r)
 
     fromList :: [a] -> [a]
     fromList xs = xs
 
+
+contract :: (a -> a -> a) -> [a] -> [a]
+contract f [] = []
+contract f [x] = [x]
+contract f (x:y:xs) = let 
+                          (l, ys) = f x y ||| rest
+                          rest = contract f xs
+                        in l : ys
+
+expand :: (a -> a -> a) -> [a] -> [a] -> [a]
+expand f xs [] = xs
+expand f xs [y] = xs
+expand f (x:xs) (y1:y2:ys) = let 
+                                ri = (f x y1)
+                                rs = expand f xs ys
+                              in x:ri:rs
